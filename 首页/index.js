92a21e023c9987c876398744f4e34df6ffe6d590
陈家浩 <2990888125@qq.com>
2019-08-01 21:31:10 +0800

    完善轮播图，分类列表

--- 首页/index.js
+++ 首页/index.js
@@ -17,7 +17,36 @@ class indexManeger {
         //渲染商品分类
 
     }
+
+    section3() {
+        //轮播图右边下面选项卡
+        var li1 = $(".bottom_img ul li");
+        li1.eq(0).css("border-bottom", "3px solid blue");
+        li1.mouseenter(function () {
+            var ind1 = $(this).index();
+            li1.eq(ind1).css("border-bottom", "3px solid blue").siblings().css("border-bottom", "none");
+            $(".ul_context").eq(ind1).css("display", "block").siblings(".ul_context").css("display", "none")
+        });
+
+        //轮播图左边列表功能
+        $(".img_left ul li").mouseenter(function () {
+            console.log($(this))
+            $(".list_context").css("display", "block");
+            if ($(this).index() == 1) {
+                // $(".li_left >div").addClass("mode2_flex").children().className("mode2 clearfix");
+                console.log($(".li_left >div").addClass("mode2_flex").children())
+                    ;
+            }
+
+        })
+        $(".img_left ul li").mouseleave(function () {
+            // console.log($(this))
+            $(".list_context").css("display", "none");
+        })
+    }
     init() {
         this.section2();
+        this.section3();
+
     }
 }
