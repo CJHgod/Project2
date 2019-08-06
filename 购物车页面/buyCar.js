da5a529293bc7041862bc2490f630ffbdc684d8a
陈家浩 <2990888125@qq.com>
2019-08-07 00:25:33 +0800

    完善购物车

--- 购物车页面/buyCar.js
+++ 购物车页面/buyCar.js
@@ -18,12 +18,79 @@ class indexManeger {
     }
 
 
-    //获取查询字符串
+    //制作购物车功能
+    buyCar() {
+        //数量增减
+        $(".num_box input").val("1");//默认1   应该传值
+        $(".shops").mouseenter(function () {
+            //加法
+            $(this).find(".num_box span").eq(1).click(function () {
+                var tes = Number($(this).siblings("input").val()) + 1
+                $(this).siblings("input").val(tes);
 
+                //同时更改总价
+                var nums = $(this).siblings("input").val();
+                var prices = $(this).parents(".num").siblings(".prices").children().text().slice(1);
+                $(this).parents(".num").siblings(".sum").children().text("¥" + nums * prices);
+                // var sums = $(this).parents(".num").siblings(".sum").children().text().slice(1);
+                console.log($(".sum span"))
+                //同时再变动合计总价
+
+                var index = Array.from($(".sum span")).length;
+                var zon = 0;
+                for (var i = 0; i < index; i++) {
+                    zon += ($(".sum span").eq(i).text().slice(1)) * 1;
+                }
+                console.log(zon)
+          
+
+                $(".rightss a").text("¥" + zon);
+            })
+
+            //减法
+            $(this).find(".num_box span").eq(0).click(function () {
+
+                var tes = Number($(this).siblings("input").val()) - 1
+                if (tes == 0) {
+                    return;
+                } else {
+                    $(this).siblings("input").val(tes);
+                }
+                //同时更改总价
+                var nums = $(this).siblings("input").val();
+                var prices = $(this).parents(".num").siblings(".prices").children().text().slice(1);
+                $(this).parents(".num").siblings(".sum").children().text("¥" + nums * prices)
+                // var sums = $(this).parents(".num").siblings(".sum").children().text().slice(1);
+                //同时再变动合计总价
+                var index = Array.from($(".sum span")).length;
+                var zon = 0;
+                for (var i = 0; i < index; i++) {
+                    zon += ($(".sum span").eq(i).text().slice(1)) * 1;
+                }
+                console.log(zon)
+          
+
+                $(".rightss a").text("¥" + zon);
+
+            })
+        })
+        $(".shops").mouseleave(function () {
+            $(this).find(".num_box span").unbind();
+        })
+
+
+
+        //商品合计监听
+
+
+        // console.log($(".rightss a").text())
+
+
+    }
 
 
     init() {
         this.section2();
-
+        this.buyCar();
     }
 }
